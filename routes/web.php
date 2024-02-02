a2b482501a388a63035c80a11b9819469b6c58d5
hamza jahangir <hamzahashmi640@gmail.com>
2024-02-02 11:39:52 +0500

    hamza

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
     return view('home');
 });
 
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
